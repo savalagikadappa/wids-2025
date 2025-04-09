3ac91a35b7c8ac5c3b9dba3ceb0d4c1991eb9c4b
kadappa savalagi <kadappasavalagi@gmail.com>
2025-04-09 13:31:59 +0530

    updated changes

--- src/components/Speakers.jsx
+++ src/components/Speakers.jsx
@@ -6,41 +6,40 @@ const speakerData = [
     id: 1,
     name: "Ms. Ashwini Chandrashekharaiah",
     image: "/ashwini-speaker2.png",
-    // Renamed `title` to `designation`:
     designation: "Senior Manager, Walmart Global Tech India",
     email: "ashwini.chandrashekharaiah@walmart.com",
     topic: "Crack the Code: Mastering the WiDS Datathon 2025",
     date: "15/04/25",
     displayDate: "15th April 2025",
     time: "4:30 PM - 5:30 PM IST",
+    linkedin: "https://www.linkedin.com/in/ashwini-chandrashekharaiah/",
   },
   {
     id: 2,
     name: "Ms. Srujana Kaddevarmuth",
     image: "/srujana-speaker1.png",
-    // Renamed `title` to `designation`:
     designation: "AI Leader, Walmart Global Tech, USA",
     email: "srujana.aba@gmail.com",
     topic: "Trends in Gen AI",
     date: "19/04/25",
     displayDate: "19th April 2025",
     time: "8:30 AM - 10:00 AM IST",
+    linkedin: "https://www.linkedin.com/in/srujana-k-37a32b18/",
   },
   {
     id: 3,
-    // Updated to show Dr. Saroja A.O
     name: "Dr. Saroja A. O",
     image: "/saroja-speaker.png",
-    // Multiline designation for Dr. Saroja
-    // Using \n for line breaks
     designation: "Professor and Head,\nDepartment of Neurology\nJ N Medical College",
     email: "sarojaaralikatte@gmail.com",
     topic: "Unraveling the Mysteries of the Female Brain",
     date: "19/04/25",
     displayDate: "19th April 2025",
     time: "10:00 AM - 11:00 AM IST",
+    // No LinkedIn
   },
 ];
+
 
 function Speakers() {
   return (
@@ -77,6 +76,16 @@ function Speakers() {
             <p>
               <strong>Email-Id:</strong> {s.email}
             </p>
+            {s.linkedin && (
+              <a
+                href={s.linkedin}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="linkedin-button"
+              >
+                View on LinkedIn
+              </a>
+            )}
           </div>
         ))}
       </div>
