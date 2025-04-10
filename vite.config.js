775b6c1ff6a02557ad36cce1e3729c522ad4dd80
kadappa savalagi <kadappasavalagi@gmail.com>
2025-04-10 14:21:34 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    host: '0.0.0.0', // Important for Render to access
+  },
 })
