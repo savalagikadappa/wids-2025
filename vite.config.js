9c7f554ced16fb2310af0cda9a4337bd0b1ed2ee
kadappa savalagi <kadappasavalagi@gmail.com>
2025-04-10 14:31:47 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
-    host: '0.0.0.0', // Important for Render to access
+    host: '0.0.0.0', // Important for external access
+    allowedHosts: [
+      'localhost',
+      'wids-2025.onrender.com', // Your Render.com domain
+      'wids.kletech.ac.in',      // Add the new hostname
+      'x.x.x.x'                 // Add the new IP address
+    ],
   },
 })
